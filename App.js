fefd9561481036c08cb96c07e1f68a2c1dc3d6ce
sthapitsrijeet <sthapitsrijeet@gmail.com>
2023-09-30 20:50:50 -0400

    Maps

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude:43.78527010823646, 
+        longitude: -79.22812082294779,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+       <Marker
+          coordinate={{
+            latitude:43.78527010823646, 
+            longitude: -79.22812082294779,
+          }}
+          title="Centennial College"
+          description="I study here"
+        />
+    </MapView>
+  </View>
   );
 }
+// AIzaSyAjXP2fQt0dJNukkJQhizU-jj0VMqT6oPM
 
 const styles = StyleSheet.create({
   container: {
